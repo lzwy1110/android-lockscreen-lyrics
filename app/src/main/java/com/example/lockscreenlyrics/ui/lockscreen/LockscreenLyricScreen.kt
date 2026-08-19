f797f65567c9be68978d75836ce258cca3a9bebc
Lzwy <liuzw1110@gmail.com>
2026-08-19 16:40:15 +0800

    fix: import androidx.compose.foundation.layout.aspectRatio

--- app/src/main/java/com/example/lockscreenlyrics/ui/lockscreen/LockscreenLyricScreen.kt
+++ app/src/main/java/com/example/lockscreenlyrics/ui/lockscreen/LockscreenLyricScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
